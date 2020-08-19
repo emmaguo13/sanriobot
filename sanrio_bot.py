4da7ec08c935e99effa6910a4b88e04549cfaab9
emmaguo13 <emmag013@yahoo.ca>
2020-08-18 21:47:10 -0700

    first test

--- sanrio_bot.py
+++ sanrio_bot.py
@@ -1,24 +1,39 @@
-#fix github
-#get personality insights
+
 #get photos
+#upload to github
 #test bot
 #release bot
-import tweepy, nltk
+import tweepy, nltk, pandas, os, time
 from textblob import TextBlob
+from ibm_watson import PersonalityInsightsV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
+#twitter credentials
 CONSUMER_KEY = 'I9tO6b8wMWeL8zcSiL73KM6sQ'
 CONSUMER_SECRET = 'jUHEiAx7NnuIa5Vz9Hy81VrQHODus2m5yyVOd5EG6moXgUlPFo'
-ACCESS_KEY = '1267989567484817410-lxX26aDMpqHM92B4SQ7dv9x3yN3sLo'
-ACCESS_SECRET = '21QzW3EedeQnW6ctGjhFJdi48gjthWoCT53ReBUcejHHF'
+ACCESS_KEY = '1267989567484817410-BpjfAOvFyyUon7EF6AYbzIpnSBoxyF'
+ACCESS_SECRET = '5qLH97G9t63JHmMQ9kKBtX8JtJiYIImVO5NIWSJSzg7MT'
 
-api = tweepy.API(auth)
+#personality credentials
+personality_api = "v0NlU-NxanvPSGaF-Dy9XmuT_7OAP3ahoJxpy3F_L0lY"
+personality_url = 'https://api.us-south.personality-insights.watson.cloud.ibm.com/instances/406c243e-5b13-4d34-810f-c33226482893'
 
+#twitter authentication
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+api = tweepy.API(auth)
 
 mentions = api.mentions_timeline() #recent mentions
 
 FILE_NAME = "last_seen_id"
+
+#personality authentication
+auth_personality = IAMAuthenticator(personality_api) #instance of personality thing
+personality_insights = PersonalityInsightsV3(version = '2-17-10-13',
+                                             authenticator = auth_personality)
+personality_insights.set_service_url(personality_url) #where our personality thing is
+
 
 
 def retrieve_last_seen_id(file_name):
@@ -53,57 +68,79 @@ def reply_to_tweets():
             print('found #mysanriokin!', flush=True)
             print('responding back...', flush=True)
             # get ID of user from tweet
-            user_id = mention.author #test to see if this returns user id once you get developer
-            character = character_from_user(user_id)
-            #if statements to update status depending on character!
+            username = mention.user.screen_name
+            path = character_from_user(username)
+            api.update_with_media(path, '@' + mention.user.screen_name + ' hi'
+                  , mention.id)
 
-            api.update_status('@' + mention.user.screen_name +
-                    '#HelloWorld back to you!', mention.id)
-
-def character_from_user(user_id):
+def character_from_user(username):
     """returns the sanrio character name based on tweet sentiments
     characters:
-    - hello kitty: frineds, crazy
-    - kuromi: edgy
-    - chococat: curious, knowledge
-    - keroppi: happy, friends
-    - cinnamoroll: help, kind,
-    - little twin stars: friends, help
-    - my melody: shy, kind
-    - pompompurin: kind, help, knowledge
-    - mimmy: pretty, shy, kind
-    - Badtz-Maru: crazy
+    - hello kitty: openness, agreeableness
+    - kuromi: challenge, excitement
+    - chococat: conscientiousness and agreeabeness, curiosity
+    - keroppi: extraversion
+    - cinnamoroll: emotion
+    - little twin stars: closeness -
+    - my melody: love, extroverted
+    - pompompurin: excitementt -
+    - mimmy: conscientiouss
     """
-    # get string of last 20 tweets
-    user_tweets = api.user_timeline(user_id)  # idk what to put in params, try to get more tweets tho
-    # user_tweets = tweepy.Cursor(api.user_timeline, id=user_id).items() ALTERNATIVE??
-    crazy = 0
-    curious = 0
-    knowledge = 0
-    help = 0
-    kind = 0
-    happy = 0
-    friends = 0
-    edgy = 0
-    shy = 0
-    pretty = 0
+    # get last 1000 tweets, join the text together
+    user_tweets = api.user_timeline(username, count = 100, include_rts = True)
+    tweets_text = [tweet.text for tweet in user_tweets]
+    joined_text = ''.join(str(tweet) for tweet in tweets_text)
 
-    for tweet in user_tweets:  # what is reversed, should i do reversed???
-        # count to see certain sentiments, certain use of words
-        positivity = TextBlob(tweet.full_text).sentiment.polarity  # find a way to read the analysis
-        #can we do autocorrect
-        # find words
-        if  'edge' in tweet.full_text or 'edgy' in in tweet.full_text:
-            edgy += 1
-        elif positivity > 0.5 or 'fun' in tweet.full_text or 'excite' in tweet.full_text or 'exciting' in tweet.full_text or 'pumped' in tweet.full_text or 'omg' in tweet.full_text or 'amazing' in tweet.full_text or 'happy' in in tweet.full_text:
-            happy += 1
-        elif 'wonder' in tweet.full_text or '?' in tweet.full_text or 'surprised' in tweet.full_text or 'huh' in tweet.full_text or 'wow' in tweet.full_text or 'curious' in tweet.full_text or 'know' in tweet.full_text:
-            curious += 1
+    #analyze the text
+    analysis_profile = personality_insights.profile(joined_text, accept ='application/json' ).get_result()
+    for personality in analysis_profile['personality']:
+        if personality['name'] == 'Openness':
+            openness = personality['percentile']
+        elif personality['name'] == 'Conscientiousness':
+            conscientiousness = personality['percentile']
+        elif personality['name'] == 'Extraversion':
+            extraversion = personality['percentile']
+        elif personality['name'] == 'Agreeableness':
+            agreeableness = personality['percentile']
+        elif personality['name'] == 'Emotional Range':
+            emotion = personality['percentile']
 
-        # if statements for how many counts, then return the character
+    for personality in analysis_profile['needs']:
+        if personality['name'] == 'Challenge':
+            challenge = personality['percentile']
+        elif personality['name'] == 'Closeness':
+            closeness = personality['percentile']
+        elif personality['name'] == 'Curiosity':
+            curiosity = personality['percentile']
+        elif personality['name'] == 'Excitement':
+            excitement = personality['percentile']
+        elif personality['name'] == 'Love':
+            love = personality['percentile']
+
+    if openness >= 0.50 and agreeableness >= 0.50:
+        return "/Users/emmaguo/sanriobot/photos/HelloKitty.jpg"
+    elif challenge >= 0.50:
+        return "/Users/emmaguo/sanriobot/photos/Kuromi.jpg"
+    elif curiosity >= 0.50:
+        return "/Users/emmaguo/sanriobot/photos/Chococat.jpg"
+    elif extraversion >= 0.50:
+        return "/Users/emmaguo/sanriobot/photos/Keroppi.png"
+    elif emotion >= 0.50:
+        return "/Users/emmaguo/sanriobot/photos/Cinnamoroll.jpg"
+    elif closeness >= 0.50:
+        return "/Users/emmaguo/sanriobot/photos/LittleTwinStars.jpg"
+    elif love >= 0.50:
+        return "/Users/emmaguo/sanriobot/photos/MyMelody.jpg"
+    elif excitement >= 0.50:
+        return "/Users/emmaguo/sanriobot/photos/Pompompurin.png"
+    elif conscientiousness >= 0.50:
+        return "/Users/emmaguo/sanriobot/photos/Mimmy.gif"
+    else:
+        return "/Users/emmaguo/sanriobot/photos/HelloKitty.jpg"
 
 
 while True: #when would it be false tho
+
     reply_to_tweets()
     time.sleep(15)
 
